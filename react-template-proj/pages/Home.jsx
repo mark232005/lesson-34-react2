7185e57fabb32e151938c4313b29087911d2ee1f
mark <alufmark@gmail.com>
2025-02-10 17:22:53 +0200

    Add img for homepage

--- react-template-proj/pages/Home.jsx
+++ react-template-proj/pages/Home.jsx
@@ -1,13 +1,11 @@
 // import {Img} from '../assets/img/Bookseller.png'
-
+import chImage from '../assets/img/ch.png'
 
 export function Home() {
 
     return (
         <section>
-            <h2>Miss Books</h2>
-            <p>Welcome</p>
-            <img src={'https://help.e-vrit.co.il/hc/article_attachments/360002006240/lending.png'} />
+            <img  className='img-home ' src={'http://127.0.0.1:5500/assets/img/ch.png'} />
         </section>
     )
 }
